3430c05194d35ae80a631f4734d11d3b05c9486e
dewmal <dewmalnilanka@gmail.com>
2016-06-08 13:22:13 +0530

    update docker file

--- Gruntfile.js
+++ Gruntfile.js
@@ -148,11 +148,11 @@ module.exports = function (grunt) {
             });
 
             // opens browser on initial server start
-            nodemon.on('config:update', function () {
+          /**  nodemon.on('config:update', function () {
               setTimeout(function () {
                 require('open')('http://localhost:8080/debug?port=5858');
               }, 500);
-            });
+            }); **/
           }
         }
       }
